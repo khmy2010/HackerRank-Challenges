28c83f89ba66bae44a55eaba09096a6e9befc33e
khmy2010 <kehan.see@gmail.com>
2018-09-03 19:54:00 +0800

    2018-09-03 19:54:00

--- publish.sh
+++ publish.sh
@@ -4,5 +4,6 @@ P_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 echo Publishing local copy "$P_DATE" to GitHub repository...
 git add .
 git commit -am "$P_DATE"
+npm version minor -m "$P_DATE"
 git push -u origin master
 echo Commit done!
