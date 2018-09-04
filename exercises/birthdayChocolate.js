243f76d62a572867dfcf9c1305b5fcbb887230ce
khmy2010 <kehan.see@gmail.com>
2018-09-04 14:11:47 +0800

    2018-09-04 14:11:46

---
+++ exercises/birthdayChocolate.js
@@ -0,0 +1,12 @@
+//link: https://www.hackerrank.com/challenges/the-birthday-bar/problem
+
+// s: an array of integer
+// d: result of sum
+// m: number of slices
+
+// first encounter to sliding window algorithm
+function birthday(s, d, m) {
+    for (let x = 0; x < s.length - 2; x++) {}
+}
+
+console.log(birthday([1, 2, 1, 3, 2], 3, 2));
---
+++ exercises/superReducedString.js
@@ -0,0 +1,49 @@
+// link: https://www.hackerrank.com/challenges/reduced-string/problem
+
+function superReducedString(s) {
+    if (s.length === 1) return s;
+    if (s.length === 0) return 'Empty String';
+
+    function isAdjacentExist(string) {
+        let exist = false;
+
+        const splited = string.split('');
+
+        splited.forEach((char, index) => {
+            const nextChar = splited[index + 1];
+            if (nextChar && char === nextChar) exist = true;
+        });
+
+        return exist;
+    }
+
+    function removeTwins(string, targetIndex) {
+        return string
+            .slice(0)
+            .split('')
+            .filter((_, index) => {
+                return index < targetIndex || index > targetIndex + 1;
+            })
+            .join('');
+    }
+
+    let result = s;
+
+    while (isAdjacentExist(result)) {
+        let targetIndex;
+        const string = result.slice(0);
+
+        for (let i = 0; i < string.length - 1; i++) {
+            const nextChar = string[i + 1];
+            if (nextChar && nextChar === string[i]) {
+                targetIndex = i;
+                break;
+            }
+        }
+        result = removeTwins(result, targetIndex);
+    }
+
+    return result.length > 0 ? result : 'Empty String';
+}
+
+console.log(superReducedString('aaabccddd'));
